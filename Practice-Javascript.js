6773af589847f30d31e70a59a4840a25beb22213
CaseyJamieson <33286974+CaseyJamieson@users.noreply.github.com>
2017-11-03 10:13:43 +0000

    Update Practice-Javascript.js

--- Practice-Javascript.js
+++ Practice-Javascript.js
@@ -126,3 +126,39 @@ for (i = 0; i < gameList.length; i++) {
 	text += "I love " + gameList[i] + "\n";
 }	
 alert (text); // Shows the list on the webpage
+
+// Start of city menu
+// Gives the cities distances from each other
+distance = new Array {
+	new Array (0, 648, 6476, 4000),
+	new Array (648, 0, 6760, 3470),
+	new Array (6476, 6760, 0, 3470),
+	new Array (4000, 3470, 5956, 0),
+		
+var theCity = ""; //Will hold the city number
+	
+// List of cities with their number	
+car cityMenu = "Please choose a city by typing a number: \n";
+cityMenu += "0) Indianapolis \n";
+cityMenu += "1) New York \n";
+cityMenu += "2) Tokyo \n";
+cityMenu += "3) London \n";
+
+// Shows the city menu
+theCity = prompt (cityMenu);
+return theCity:
+}
+
+// Works out the distance between cities
+function main() {
+	var output = "";
+	var from = getCity ();
+	var to = getCity();
+	var result = distance[from][to];
+	output = "The distance from " + cityName[from];
+	output += " to " + cityName[to];
+	output += " is " + result + " miles.";  // Tells you the answer in a sentence
+}
+main()
+// End of city menu	
+	
